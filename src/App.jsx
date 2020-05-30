522cfc1a1a003103c3cea150d47a092d3ab1d212
benjaminfmoore <benjaminfmoore@yahoo.com>
2020-05-30 17:26:04 -0400

    path fixing

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
             <Route path="/articles/new" component={ArticleForm} />
             <Route path="/articles/:articleId/edit" component={ArticleForm} />
             <Route path="/articles/:articleId" component={Article} />
-            <Route path="/articles" component={Articles} />
+            <Route path="/" component={Articles} />
           </Switch>
         </Container>
       </BrowserRouter>
